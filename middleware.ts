029565cfb381363daa7fca640e6c8cd0558771e2
Rajput Amit <rajputamitsinh.v.092@gamil.com>
2023-12-21 15:35:54 +0530

    Modified_Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,19 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-    publicRoutes:[
-        '/',
-        '/events/:id',
-        '/api/webhook/Clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing',
-    ],
-    ignoredRoutes:[
-         '/api/webhook/Clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing',
-    ]
+  publicRoutes: [
+    "/",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+    "/api/webhook/clerk(.*)",
+  ],
+  ignoredRoutes: [
+    "/api/webhook/Clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
 });
  
 export const config = {
